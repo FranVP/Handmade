55406b5b29ced010986a0519517e46ab3c137056
Fran Villarreal <Fran_villarreal119@hotmail.com>
2017-01-28 19:16:27 +0100

    fix bugs busquedas admin

--- Galeria_admin/index.php
+++ Galeria_admin/index.php
@@ -6,7 +6,6 @@ $resultado = mysqli_query($conexion, "SELECT ID_GALERIA, TITULO, DESCRIPCION, FE
 <html>
 <head lang="es">
     <meta charset="UTF-8">
-    <meta charset="UTF-8">
     <title>Admin Galerias</title>
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
 </head>
@@ -23,19 +22,28 @@ $resultado = mysqli_query($conexion, "SELECT ID_GALERIA, TITULO, DESCRIPCION, FE
 </form>
 
 <?php
-$busca='';
+$busca="";
+mysql_connect('localhost','root');
+mysql_select_db("handmade");
+
+
 $busca= isset($_POST['busca'])?$_POST['busca']:NUll;
 if($busca!=""){
-    $busqueda = mysqli_query
-    ($conexion,"
-    SELECT ID_GALERIA, TITULO, DESCRIPCION, FECHA_ALTA
+    $busqueda=mysql_query("SELECT ID_GALERIA,TITULO, DESCRIPCION, FECHA_ALTA
     from galerias
-    where titulo like '%".$busca."%'")
+    where TITULO like '%".$busca."%'")
     or die("Error: ".mysqli_error($conexion));;
 }
 ?>
-<h2>Listdo de galerias buscadas</h2>
-<div class="col-md-6">
+
+
+
+
+
+
+
+    <h2>Listdo de galerias buscadas</h2>
+<div class="col-md-12">
     <table class="table table-condensed">
     <thead>
     <tr>
@@ -46,12 +54,12 @@ if($busca!=""){
     </tr>
     </thead>
 <?php
-while( $fila1 = mysqli_fetch_assoc( isset($busqueda)?$_GET[$busqueda]:NUll)){
+while( $fila1 = mysql_fetch_array ($busqueda)){
     echo "<tr>";
-    echo "<td>.$fila1[TITULO]</td>";
-    echo "<td>.$fila1[DESCRIPCION]</td>";
-    echo "<td>.$fila1[FECHA_ALTA]</td>";
-    echo "<td> <a href='editar_galeria.php?id=.$fila1[ID_GALERIA]'>editar</a> | <a href='borrar_galeria.php?id=.$fila1[ID_GALERIA]'>borrar </a>|<a href='administrar_fotos.php?id=.$fila1[ID_GALERIA]'> administrar fotos</a></td>";
+    echo "<td>$fila1[TITULO]</td>";
+    echo "<td>$fila1[DESCRIPCION]</td>";
+    echo "<td>$fila1[FECHA_ALTA]</td>";
+    echo "<td> <a href='editar_galeria.php?id=$fila1[ID_GALERIA]'>editar</a> | <a href='borrar_galeria.php?id=$fila1[ID_GALERIA]'>borrar </a>|<a href='administrar_fotos.php?id=.$fila1[ID_GALERIA]'> administrar fotos</a></td>";
     echo "<tr>";
 }
 ?>
@@ -63,7 +71,7 @@ while( $fila1 = mysqli_fetch_assoc( isset($busqueda)?$_GET[$busqueda]:NUll)){
 
 <h2>Listdo de galerias</h2>
 <div><a href="nueva_galeria.php"> Crear una galeria</a> </div>
-    <div class="col-md-6">
+    <div class="col-md-12">
 <table class="table table-condensed">
     <tr>
         <th>Nombre</th>
