3dbb3e34aae2f56a5119fac3c458a21536f8fbc7
Fran Villarreal <Fran_villarreal119@hotmail.com>
2017-01-28 21:01:42 +0100

    implementando Navbar en Home

--- home.php
+++ home.php
@@ -1,17 +1,43 @@
 <html>
-<head>
+<head lang="es">
+    <meta charset="UTF-8">
     <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
-<body bgcolor="#6b8e23">
-<br />
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Cambiar Navegacion</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">HANDMADE PRESENTS</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                        <li><a href="nosotros.php">Nosotros</a></li>
+                        <li><a href="pedidos.php">Pedidos</a></li>
+                        <li><a href="galeria.php">Galeria</a></li>
+                </ul>
+                <!-- <form class="navbar-form navbar-left">
+                     <div class="form-group">
+                         <input type="text" class="form-control"></div>
+                     <button type="submit" class="btn btn-default">Buscar</button>
+                 </form>-->
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </div><!-- /.container-fluid -->
+    </nav>
+</div>
+    <script src="js/jquery.js"></script>
 </body>
 </html>
-
-<TABLE id="menu" WIDTH="50%" BORDER="1" CELLSPACING="3" CELLPADDING="2">
-    <TR>
-        <TD COLSPAN="4"><a href="galeria.php">Galeria</a></TD>
-        <TD><a href="nosotros.php">Nosotros</a></TD>
-        <TD><a href="pedidos.php">Pedidos</a></TD>
-        <TD><a href="login.php">Login</a></TD>
-    </TR>
-</TABLE>
