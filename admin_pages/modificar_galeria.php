24eff9fe8f74de54f998ecc57cab5efe38fdbaea
Fran Villarreal <Fran_villarreal119@hotmail.com>
2018-05-12 18:53:53 +0200

    cambiar redirecciones al borrar o modificar

--- admin_pages/modificar_galeria.php
+++ admin_pages/modificar_galeria.php
@@ -4,5 +4,5 @@ $titulo = $_POST['titulo'];
 $descripcion = $_POST['descripcion'];
 $id = $_POST['id_galeria'];
 $result = mysqli_query($conexion, "UPDATE galerias SET TITULO='$titulo', DESCRIPCION='$descripcion' WHERE ID_GALERIA  = '$id'")or die("Error: ".mysqli_error($conexion));;
-header('Location: index.php');
+header('Location: admin_galerias.php');
 ?>
